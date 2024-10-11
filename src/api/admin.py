66ea91e8c56e134ae0999c0eeda657b076be7b95
Cristian Aravena <50273105+crisstrance@users.noreply.github.com>
2024-10-11 19:23:15 +0000

    EDIT - models

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
 import os
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from .models import db, Users
+from .models import db, Users, Posts, Teams, Players, Seasons
 
 
 def setup_admin(app):
@@ -9,5 +9,8 @@ def setup_admin(app):
     app.config['FLASK_ADMIN_SWATCH'] = 'darkly'
     admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
     admin.add_view(ModelView(Users, db.session))  # You can duplicate that line to add mew models
-    # admin.add_view(ModelView(YourModelName, db.session))
+    admin.add_view(ModelView(Posts, db.session))
+    admin.add_view(ModelView(Teams, db.session))
+    admin.add_view(ModelView(Players, db.session))
+    admin.add_view(ModelView(Seasons, db.session))
     
