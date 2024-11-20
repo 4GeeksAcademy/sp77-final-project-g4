1afa82d4c47859e50470069d5caff9dc398ad971
Cristian Aravena <50273105+crisstrance@users.noreply.github.com>
2024-11-20 16:50:30 +0000

    FIX stats

--- src/front/js/component/PlayerStats.jsx
+++ src/front/js/component/PlayerStats.jsx
@@ -35,77 +35,122 @@ const PlayerStats = () => {
         return <div className="alert alert-danger text-center">{error}</div>;
     }
 
-    if (!store.playerStats || store.playerStats.length === 0) {
+    // Verificar si los datos están disponibles
+    const { playerName, stats } = store.playerStats || {};
+    if (!stats || stats.length === 0) {
         return <div className="text-center my-4">No stats found for this player.</div>;
     }
 
-    // Extrae las estadísticas del jugador
-    const playerStats = store.playerStats;
-
     return (
         <div className="container my-5">
-            <h2 className="text-center text-dark mb-4">Player Stats for {params.id}</h2>
-
-            <table className="table bg-white table-hover text-center border-danger">
-                <thead className="table-dark">
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Jugador</th>
-                        <th scope="col">Puntos</th>
-                        <th scope="col">Asistencias</th>
-                        <th scope="col">Rebotes Totales</th>
-                        <th scope="col">Rebotes Ofensivos</th>
-                        <th scope="col">Rebotes Defensivos</th>
-                        <th scope="col">Tiros de Campo</th>
-                        <th scope="col">Tiros Intentados</th>
-                        <th scope="col">% Tiros de Campo</th>
-                        <th scope="col">Tiros de 3</th>
-                        <th scope="col">Tiros de 3 Intentados</th>
-                        <th scope="col">% Tiros de 3</th>
-                        <th scope="col">Tiros de 2</th>
-                        <th scope="col">Tiros de 2 Intentados</th>
-                        <th scope="col">% Tiros de 2</th>
-                        <th scope="col">% Eficiencia Tiros de Campo</th>
-                        <th scope="col">Tiros Libres</th>
-                        <th scope="col">Tiros Libres Intentados</th>
-                        <th scope="col">% Tiros Libres</th>
-                        <th scope="col">Turnovers</th>
-                        <th scope="col">Faltas Personales</th>
-                        <th scope="col">Juegos Iniciados</th>
-                        <th scope="col">Minutos por Juego</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {playerStats.map((stats, index) => (
-                        <tr key={stats.id}>
-                            <th scope="row">{index + 1}</th>
-                            <td className="text-start">{stats.player_id}</td> {/* O el nombre del jugador si está disponible */}
-                            <td>{stats.points}</td>
-                            <td>{stats.assists}</td>
-                            <td>{stats.total_rb}</td>
-                            <td>{stats.offensive_rb}</td>
-                            <td>{stats.defensive_rb}</td>
-                            <td>{stats.field_goals}</td>
-                            <td>{stats.field_attempts}</td>
-                            <td>{stats.field_percent}</td>
-                            <td>{stats.three_fg}</td>
-                            <td>{stats.three_attempts}</td>
-                            <td>{stats.three_percent}</td>
-                            <td>{stats.two_fg}</td>
-                            <td>{stats.two_attempts}</td>
-                            <td>{stats.two_percent}</td>
-                            <td>{stats.effect_fg_percent}</td>
-                            <td>{stats.ft}</td>
-                            <td>{stats.ft_attempts}</td>
-                            <td>{stats.ft_percent}</td>
-                            <td>{stats.turnovers}</td>
-                            <td>{stats.personal_fouls}</td>
-                            <td>{stats.games_started}</td>
-                            <td>{stats.minutes_pg}</td>
+            <h2 className="text-center text-dark mb-4">Stats de {playerName}</h2>
+            <div className="container">
+                <table className="table bg-white table-hover text-center border-danger">
+                    <thead className="table-dark">
+                        <tr>
+                            <th scope="col" className="text-start">Estadísticas</th>
+                            <th scope="col">Valores</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {stats.map((stat, index) => (
+                            <React.Fragment key={stat.id}>
+                                <tr>
+                                    <td className="text-start"><strong>Puntos</strong></td>
+                                    <td>{stat.points}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Asistencias</strong></td>
+                                    <td>{stat.assists}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Rebotes Totales</strong></td>
+                                    <td>{stat.total_rb}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Rebotes Ofensivos</strong></td>
+                                    <td>{stat.offensive_rb}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Rebotes Defensivos</strong></td>
+                                    <td>{stat.defensive_rb}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Tiros de Campo</strong></td>
+                                    <td>{stat.field_goals}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Tiros Intentados</strong></td>
+                                    <td>{stat.field_attempts}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>% Tiros de Campo</strong></td>
+                                    <td>{stat.field_percent}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Tiros de 3</strong></td>
+                                    <td>{stat.three_fg}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Tiros de 3 Intentados</strong></td>
+                                    <td>{stat.three_attempts}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>% Tiros de 3</strong></td>
+                                    <td>{stat.three_percent}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Tiros de 2</strong></td>
+                                    <td>{stat.two_fg}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Tiros de 2 Intentados</strong></td>
+                                    <td>{stat.two_attempts}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>% Tiros de 2</strong></td>
+                                    <td>{stat.two_percent}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>% Eficiencia Tiros de Campo</strong></td>
+                                    <td>{stat.effect_fg_percent}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Tiros Libres</strong></td>
+                                    <td>{stat.ft}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Tiros Libres Intentados</strong></td>
+                                    <td>{stat.ft_attempts}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>% Tiros Libres</strong></td>
+                                    <td>{stat.ft_percent}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Turnovers</strong></td>
+                                    <td>{stat.turnovers}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Faltas Personales</strong></td>
+                                    <td>{stat.personal_fouls}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Juegos Iniciados</strong></td>
+                                    <td>{stat.games_started}</td>
+                                </tr>
+                                <tr>
+                                    <td className="text-start"><strong>Minutos por Juego</strong></td>
+                                    <td>{stat.minutes_pg}</td>
+                                </tr>
+                                <tr>
+                                    <td colSpan="2" className="table-dark"></td> {/* Separador visual entre jugadores */}
+                                </tr>
+                            </React.Fragment>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
